ce75542c143fff0faf73244eaa25dc99630e7b7f
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-11-16 23:13:10 +0530

    Removed all oauth setup.

--- lib/common/utils/constants.dart
+++ lib/common/utils/constants.dart
@@ -1,7 +1,1 @@
-const WAKATIME_URL = "https://wakatime.com";
-const WAKATIME_CLIENT_ID = "UKQ9xvpSflsXL1dS7dgMa6h3";
-const WAKATIME_CLIENT_SECRET =
-    "sec_k8JY1YIxKGtz51Br3bzKZKYNT6PfLXvJIaXaG95rzVfDQ1snFN8o2lXD7VK2t4Ckt1fQVghvUHCGu1nU";
-const WAKATIME_AUTH_ENDPOINT = "https://wakatime.com/oauth/authorize";
-const WAKATIME_TOKEN_ENDPOINT = "https://wakatime.com/oauth/token";
-const WAKATIME_REDIRECT_URI = "wakatimeapp://oauth2redirect";
+
--- lib/features/login/ui/login_page.dart
+++ lib/features/login/ui/login_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:oauth2_client/access_token_response.dart';
-import 'package:oauth2_client/oauth2_client.dart';
-import 'package:waka_time_app/common/utils/constants.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,11 +8,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  var client = MyOAuth2Client(
-    redirectUri: WAKATIME_REDIRECT_URI,
-    customUriScheme: "com.jacob.wakatimeapp",
-  );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,27 +25,5 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Future<void> onClick() async {
-    AccessTokenResponse accessTokenResponse =
-        await client.getTokenWithAuthCodeFlow(
-      clientId: WAKATIME_CLIENT_ID,
-      clientSecret: WAKATIME_CLIENT_SECRET,
-      scopes: ["email", "read_stats"],
-      enablePKCE: false,
-    );
-    print(accessTokenResponse.accessToken);
-    print(accessTokenResponse.expiresIn);
-  }
-}
-
-class MyOAuth2Client extends OAuth2Client {
-  MyOAuth2Client({required String redirectUri, required String customUriScheme})
-      : super(
-          authorizeUrl: WAKATIME_AUTH_ENDPOINT,
-          tokenUrl: WAKATIME_TOKEN_ENDPOINT,
-          redirectUri: redirectUri,
-          customUriScheme: customUriScheme,
-          credentialsLocation: CredentialsLocation.BODY,
-          scopeSeparator: ",",
-        );
+  void onClick() async {}
 }
