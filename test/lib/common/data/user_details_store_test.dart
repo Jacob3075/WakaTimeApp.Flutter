f0d13ba1df2d589dd2577b83acc51dbd575866ce
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-01-04 21:28:55 +0530

    Added tests for UserDetailsStore.

---
+++ test/lib/common/data/user_details_store_test.dart
@@ -0,0 +1,84 @@
+import "package:flutter_secure_storage/flutter_secure_storage.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:mockito/annotations.dart";
+import "package:mockito/mockito.dart";
+import "package:waka_time_app/common/data/user_details_store.dart";
+import "package:waka_time_app/common/domain/models/user_details.dart";
+
+import "../../fixtures/cached_user_details.dart";
+import "user_details_store_test.mocks.dart";
+
+@GenerateMocks([FlutterSecureStorage])
+main() {
+  late MockFlutterSecureStorage mockStorage;
+  late UserDetailsStore store;
+
+  setUp(() {
+    mockStorage = MockFlutterSecureStorage();
+    store = UserDetailsStore(storage: mockStorage);
+  });
+
+  const userDetailsKey = "KEY_USER_DETAILS";
+  const apiKeyKey = "KEY_API_KEY";
+
+  group(
+    "When store contains values",
+    () {
+      test(
+        "for user details reading it should return a valid object",
+        () async {
+          when(mockStorage.read(key: userDetailsKey))
+              .thenAnswer((realInvocation) async => cachedUserDetailsJson);
+
+          UserDetails? userDetails = await store.getUserDetails();
+
+          expect(userDetails, isNotNull);
+          expect(userDetails, equals(userDetails));
+        },
+      );
+
+      test(
+        "for api key then reading it should return the key",
+        () async {
+          var apiKey = "e15618f1-bbfc-42e9-9fb9-c4a82e0b23ec";
+          when(mockStorage.read(key: apiKeyKey))
+              .thenAnswer((realInvocation) async => apiKey);
+
+          String? apiKeyFromStore = await store.getApiKey();
+
+          expect(apiKeyFromStore, isNotNull);
+          expect(apiKeyFromStore, equals(apiKey));
+        },
+      );
+    },
+  );
+
+  group(
+    "When store does not contain values",
+    () {
+      test(
+        "for user details reading it should return null",
+        () async {
+          when(mockStorage.read(key: userDetailsKey))
+              .thenAnswer((realInvocation) async => null);
+
+          UserDetails? userDetails = await store.getUserDetails();
+
+          expect(userDetails, isNull);
+        },
+      );
+
+      test(
+        "for api key then reading it should return null",
+        () async {
+          when(mockStorage.read(key: apiKeyKey))
+              .thenAnswer((realInvocation) async => null);
+
+          String? apiKeyFromStore = await store.getApiKey();
+
+          expect(apiKeyFromStore, isNull);
+        },
+      );
+    },
+  );
+}
---
+++ test/lib/fixtures/cached_user_details.dart
@@ -0,0 +1,35 @@
+import "package:waka_time_app/common/domain/models/user_details.dart";
+
+const cachedUserDetailsJson = """{
+  "bio": "abc",
+  "email": "jacob.bosco3075@gmail.com",
+  "id": "0ace2da6-e00b-4441-ae74-7638ad8fb5ad",
+  "timeout": 15,
+  "timezone": "Asia/Kolkata",
+  "username": "Jacob3075",
+  "displayName": "jacob",
+  "lastProject": "Waka Time App",
+  "fullName": "Jacob",
+  "durationsSliceBy": "Language",
+  "createdAt": "2020-05-18T13:28:23Z",
+  "dateFormat": "YYYY-MM-DD",
+  "photoUrl": "https://wakatime.com/photo/6718342a-2e14-4b77-8f65-a04226794b98?s420"
+}
+""";
+
+const userDetails = UserDetails(
+  bio: "abc",
+  email: "jacob.bosco3075@gmail.com",
+  id: "0ace2da6-e00b-4441-ae74-7638ad8fb5ad",
+  timeout: 15,
+  timezone: "Asia/Kolkata",
+  username: "Jacob3075",
+  displayName: "jacob",
+  lastProject: "Waka Time App",
+  fullName: "Jacob",
+  durationsSliceBy: "Language",
+  createdAt: "2020-05-18T13:28:23Z",
+  dateFormat: "YYYY-MM-DD",
+  photoUrl:
+      "https://wakatime.com/photo/6718342a-2e14-4b77-8f65-a04226794b98?s420",
+);
