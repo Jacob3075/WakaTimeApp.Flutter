f2ee901d2da2020641ea3387cbf6d0c7e1fce874
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-01-12 11:11:14 +0530

    Updated app_assets file to have new animations.

--- lib/common/ui/theme/app_assets.dart
+++ lib/common/ui/theme/app_assets.dart
@@ -1,6 +1,7 @@
 abstract class AppAssets {
   static const icons = _Icons();
   static const images = _Images();
+  static const animations = _Animation();
 }
 
 class _Icons {
@@ -20,4 +21,30 @@ class _Images {
   const _Images();
 
   final launcherIcon = "assets/images/ic_launcher.png";
+}
+
+class _Animation {
+  const _Animation();
+
+  final blueLoading = "assets/animations/blue_loading.json";
+  final dotsAndLinesLoadingPlexus = "assets/animations/dots_and_lines_loading_plexus.json";
+  final emptyBox1 = "assets/animations/empty_box_1.json";
+  final emptyBox2 = "assets/animations/empty_box_2.json";
+  final empty = "assets/animations/empty.json";
+  final error1 = "assets/animations/error_1.json";
+  final error2 = "assets/animations/error_2.json";
+  final errorAnimation = "assets/animations/error_animation.json";
+  final expandingUiElements = "assets/animations/expanding_ui_elements.json";
+  final internet = "assets/animations/internet.json";
+  final jumpingBoxOnRopeLoadingAnimation =
+      "assets/animations/jumping_box_on_rope_loading_animation.json";
+  final liquidBlobLoaderGreen = "assets/animations/liquid_blob_loader_green.json";
+  final loading1 = "assets/animations/loading_1.json";
+  final loading2 = "assets/animations/loading_2.json";
+  final loadingAnimation = "assets/animations/loading_animation.json";
+  final loadingBlob = "assets/animations/loading_blob.json";
+  final loadingPaperPlane1 = "assets/animations/loading_paper_plane_1.json";
+  final loopLoadingAnimation = "assets/animations/loop_loading_animation.json";
+  final noMobileInternet = "assets/animations/no_mobile_internet.json";
+  final worldLocations = "assets/animations/world_locations.json";
 }
