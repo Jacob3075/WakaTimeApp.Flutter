38798f90a5b12f56a4c1c114680a23fab9f6fcae
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-01-25 15:56:35 +0530

    Update header_section to be an appbar and updated detailed_project_stats with tab bar setup.

--- lib/features/project_stats/ui/detailed_project_stats.dart
+++ lib/features/project_stats/ui/detailed_project_stats.dart
@@ -1,14 +1,11 @@
-import "package:auto_route/auto_route.dart";
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
-import "package:flutter_screenutil/flutter_screenutil.dart";
 import "package:waka_time_app/common/domain/errors/errors.dart";
 import "package:waka_time_app/common/domain/models/summaries.dart";
 import "package:waka_time_app/common/ui/widgets/loading_animation.dart";
 import "package:waka_time_app/di/injection.dart";
 import "package:waka_time_app/features/project_stats/ui/bloc/detailed_project_stats_bloc.dart";
-import "package:waka_time_app/features/project_stats/ui/widgets/header_section.dart";
-import "package:waka_time_app/features/project_stats/ui/widgets/stats_pager.dart";
+import "package:waka_time_app/features/project_stats/ui/widgets/custom_app_bar.dart";
 
 typedef _Event = DetailedProjectStatsEvent;
 
@@ -21,36 +18,44 @@ class DetailedProjectStats extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: BlocProvider(
-          create: (context) =>
-              getIt<DetailedProjectStatsBloc>()..add(_Event.loadData(projectName: projectName)),
-          child: BlocBuilder<DetailedProjectStatsBloc, DetailedProjectStatsState>(
-            builder: (_, state) => state.when(
-              loading: _onLoadingState,
-              dataLoaded: (it) => _onDataLoadedState(it, context),
-              error: _onErrorSate,
-            ),
+  Widget build(BuildContext context) => BlocProvider(
+        create: (context) =>
+            getIt<DetailedProjectStatsBloc>()..add(_Event.loadData(projectName: projectName)),
+        child: BlocBuilder<DetailedProjectStatsBloc, DetailedProjectStatsState>(
+          builder: (_, state) => state.when(
+            loading: _onLoadingState,
+            dataLoaded: (it) => _onDataLoadedState(it, context),
+            error: _onErrorSate,
+          ),
+        ),
+      );
+
+  Widget _onDataLoadedState(Summaries projectStats, BuildContext context) {
+    return SafeArea(
+      child: DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: CustomAppBar(projectName: projectName),
+          body: const TabBarView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              Text("Page 1"),
+              Text("Page 2"),
+              Text("Page 3"),
+              Text("Page 4"),
+              Text("Page 5"),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget _onDataLoadedState(Summaries projectStats, BuildContext context) => Column(
-        children: [
-          SizedBox(height: 20.h),
-          HeaderSection(
-            projectName: projectName,
-            onClick: () => context.router.pop(),
-          ),
-          const StatsPager(),
-        ],
+  Widget _onLoadingState() => const Scaffold(
+        body: SafeArea(
+          child: LoadingAnimation(),
+        ),
       );
-
-  Widget _onLoadingState() => const LoadingAnimation();
 
   // TODO: ADD ERROR SCREEN
   Widget _onErrorSate(Errors errors) => Center(child: Text("ERROR: $errors"));
---
+++ lib/features/project_stats/ui/widgets/custom_app_bar.dart
@@ -0,0 +1,34 @@
+import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:waka_time_app/common/ui/theme/app_colors.dart";
+
+class CustomAppBar extends AppBar {
+  CustomAppBar({
+    Key? key,
+    projectName,
+  }) : super(
+          key: key,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            projectName,
+            style: TextStyle(
+              fontSize: 30.sp,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          titleSpacing: 0,
+          leading: Icon(Icons.arrow_back_ios, size: 28.sp),
+          bottom: const TabBar(
+            indicatorColor: AppColors.accentIcons,
+            isScrollable: true,
+            tabs: [
+              Tab(text: "Time"),
+              Tab(text: "Language"),
+              Tab(text: "OS"),
+              Tab(text: "Editor"),
+              Tab(text: "Other"),
+            ],
+          ),
+        );
+}
