325f4ecb7b5e51dd7ce52c82ef022b4d925b411f
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-02-04 23:09:54 +0530

    Added function to get reduced list of languages used in project and added test for it.

--- lib/features/project_stats/domain/models/project_summaries.dart
+++ lib/features/project_stats/domain/models/project_summaries.dart
@@ -1,6 +1,7 @@
 import "package:freezed_annotation/freezed_annotation.dart";
 import "package:waka_time_app/common/domain/models/common_models.dart";
 import "package:waka_time_app/common/domain/models/time.dart";
+import 'package:waka_time_app/common/utils/extensions.dart';
 import "package:waka_time_app/features/project_stats/domain/models/daily_project_stats.dart";
 
 part "project_summaries.freezed.dart";
@@ -24,6 +25,22 @@ class ProjectSummaries with _$ProjectSummaries {
     final totalDays = dailyProjectStats.where((element) => element.timeSpent.decimal != 0).length;
     return DaysWorked(months: totalDays ~/ 30, days: totalDays % 30);
   }
+
+  List<Language> languagesUsed() => dailyProjectStats
+      .expand((it) => it.languages)
+      .groupFoldBy(
+        (it) => it.name,
+        (it1, it2) {
+          final timeSpent = ((it1 as Language?)?.timeSpent ?? Time.zero) + it2.timeSpent;
+          return Language(
+            name: (it2).name,
+            timeSpent: timeSpent,
+            percent: ((timeSpent.decimal / totalTime.decimal) * 100).roundToDecimal(2),
+          );
+        },
+      )
+      .values
+      .toList();
 }
 
 @freezed
---
+++ test/lib/features/project_stats/domain/models/project_summaries_test.dart
@@ -0,0 +1,77 @@
+import "package:flutter_test/flutter_test.dart";
+import "package:waka_time_app/common/domain/models/common_models.dart";
+import "package:waka_time_app/common/domain/models/time.dart";
+import "package:waka_time_app/features/project_stats/domain/models/daily_project_stats.dart";
+import "package:waka_time_app/features/project_stats/domain/models/project_summaries.dart";
+
+main() {
+  test("languages used function returns stats for all the languages used in the project", () {
+    final dailyProjectStats = DailyProjectStats(
+      timeSpent: Time.zero,
+      entities: List.empty(),
+      languages: const [
+        Language(
+          name: "L1",
+          timeSpent: Time(hours: 2, minutes: 30, decimal: 2.5),
+          percent: 18.51,
+        ),
+        Language(
+          name: "L2",
+          timeSpent: Time(hours: 1, minutes: 15, decimal: 1.25),
+          percent: 9.25,
+        ),
+        Language(
+          name: "L3",
+          timeSpent: Time(hours: 3, minutes: 0, decimal: 3.0),
+          percent: 22.22,
+        ),
+        Language(
+          name: "L1",
+          timeSpent: Time(hours: 2, minutes: 30, decimal: 2.5),
+          percent: 18.51,
+        ),
+        Language(
+          name: "L1",
+          timeSpent: Time(hours: 1, minutes: 15, decimal: 1.25),
+          percent: 9.25,
+        ),
+        Language(
+          name: "L2",
+          timeSpent: Time(hours: 3, minutes: 0, decimal: 3.0),
+          percent: 22.22,
+        ),
+      ],
+      editors: List.empty(),
+      operatingSystems: List.empty(),
+      date: DateTime.now(),
+    );
+    final ProjectSummaries projectSummaries = ProjectSummaries(
+      totalTime: const Time(hours: 27, minutes: 0, decimal: 27),
+      dailyProjectStats: [dailyProjectStats, dailyProjectStats],
+      range: StatsRange(
+        startDate: DateTime.now(),
+        endDate: DateTime.now(),
+      ),
+    );
+    expect(
+      projectSummaries.languagesUsed(),
+      const [
+        Language(
+          name: "L1",
+          timeSpent: Time(hours: 12, minutes: 30, decimal: 12.5),
+          percent: 46.3,
+        ),
+        Language(
+          name: "L2",
+          timeSpent: Time(hours: 8, minutes: 30, decimal: 8.5),
+          percent: 31.48,
+        ),
+        Language(
+          name: "L3",
+          timeSpent: Time(hours: 6, minutes: 0, decimal: 6.0),
+          percent: 22.22,
+        ),
+      ],
+    );
+  });
+}
