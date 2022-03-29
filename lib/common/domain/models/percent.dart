441a5e298987deb15124eebb80e3e8aee2a27c95
Jacob3075 <jacob.bosco3075@gmail.com>
2022-03-29 17:55:14 +0530

    Fixed issue in Percent.+ where numerator becomes larger than denominator.

--- lib/common/domain/models/percent.dart
+++ lib/common/domain/models/percent.dart
@@ -13,10 +13,22 @@ class Percent {
 
   static const Percent zero = Percent(0, 0);
 
-  /// Added the [numerator] values of this and [other],
-  /// Finds max of [denominator] and [other.denominator] because [Percent.zero] can be added sometimes
-  Percent operator +(Percent other) =>
-      Percent(numerator + other.numerator, max(denominator, other.denominator));
+  /// Adds [other.numerator] with [numerator].
+  ///
+  /// Finds max of [denominator] and [other.denominator] because [Percent.zero] can be added sometimes.
+  ///
+  /// Adds the [numerator] values of this and [other.numerator], if sum is greater than the [newDenominator]
+  /// sets the value of [newNumerator] to [newDenominator]
+  ///
+  /// Extra logic because sum of the 2 numerators can sometimes be larger than the denominator
+  ///
+  /// FIXME: HOW IS VALUE SUM OF NUMERATORS GREATER THAN THE DENOMINATOR
+  Percent operator +(Percent other) {
+    final newDenominator = max(denominator, other.denominator);
+    final numeratorSum = numerator + other.numerator;
+    final newNumerator = min(numeratorSum, newDenominator);
+    return Percent(newNumerator, newDenominator);
+  }
 
   Percent merge(Percent other) =>
       Percent(numerator + other.numerator, denominator + other.denominator);
