47626f5cb39fc7dd86d477acd6751ecc0dcc3ac3
Jacob3075 <jacob.bosco3075@gmail.com>
2022-04-08 21:20:36 +0530

    Added basic UI for search_projects page.

--- lib/features/search_projects/ui/search_projects.dart
+++ lib/features/search_projects/ui/search_projects.dart
@@ -1,10 +1,38 @@
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
+import "package:waka_time_app/common/domain/errors/errors.dart";
+import "package:waka_time_app/common/domain/models/project_details.dart";
+import "package:waka_time_app/di/injection.dart";
+import "package:waka_time_app/features/search_projects/ui/bloc/search_projects_bloc.dart";
+
+typedef _E = SearchProjectsEvent;
 
 class SearchProjects extends StatelessWidget {
   const SearchProjects({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: SafeArea(
+          child: BlocProvider(
+            create: (context) => getIt<SearchProjectsBloc>()..add(const _E.loadData()),
+            child: BlocBuilder<SearchProjectsBloc, SearchProjectsState>(
+              builder: (_, state) => state.when(
+                loading: _onLoadingState,
+                dataLoaded: (it) => _onDataLoadedState(it, context),
+                error: _onErrorSate,
+              ),
+            ),
+          ),
+        ),
+      );
+
+  Widget _onLoadingState() => const Text("LOADING");
+
+  Widget _onDataLoadedState(List<ProjectDetails> projects, BuildContext context) =>
+      ListView.builder(
+        itemCount: projects.length,
+        itemBuilder: (BuildContext context, int index) => Text(projects[index].projectName),
+      );
+
+  Widget _onErrorSate(Errors error) => Text(error.getErrorMessage());
 }
